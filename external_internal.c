2ac939c6c2023bfde5ce329a0462f1004f873502
mydhilimanoj <115702644+mydhilimanoj@users.noreply.github.com>
2023-02-17 11:14:15 +0530

    Add files via upload

---
+++ external_internal.c
@@ -0,0 +1,16 @@
+<html>
+<head>
+<link rel="stylesheet" href="/home/mcalab/Desktop/Arya/style.css">
+<style>
+p2{
+font-size:50px;
+}
+</style>
+<body>
+<p1 style="color:blue;font-family:verdana;font-size300%;">this is inline css method</p1>
+<br>
+<p2>this is internal css method</p2>
+<br>
+<p>this is a paragraph of external css </p>
+</body>
+</html>
