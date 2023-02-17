e13995fadcc74f40eb94ba687de64f3cba7e53af
mydhilimanoj <115702644+mydhilimanoj@users.noreply.github.com>
2023-02-17 09:45:00 +0530

    Add files via upload

---
+++ my_sql4.php
@@ -0,0 +1,13 @@
+
+<?php
+$con=mysqli_connect("localhost","root","","sample");
+$id=$_GET["id"];
+if($con)
+{
+$qry1="delete from studentname where id='$id'";
+if(mysqli_query($con, $qry1))
+{
+echo "Data Removed<br>";
+}
+}
+?>
---
+++ players.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$players = array("Dhoni", "Suresh raina", "Virat Kohli","Ravindra jadeja","Rohit sharma"); 
+echo "I like " . $players[0] . ", " . $players[1] . "," . $playes[2] .$players[3] . "," . $players[4] . "." .;
+?>
+
+</body>
+</html>
+
